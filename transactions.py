be634bddd29cfd4c9169760c4705ae0652e5b6bb
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-10-07 16:00:17 -0400

    implements function to add transactions to transactions table

--- transactions.py
+++ transactions.py
@@ -6,3 +6,13 @@ def get_transactions(conn):
     cur = conn.cursor()
     cur.execute("SELECT * FROM transactions")
     return cur.fetchall()
+
+# adds transaction as a tuple to transactions table in database
+
+def add_transaction(conn, transaction):
+    sql = '''INSERT INTO transactions(type, category, amount, date)
+              VALUES(?, ?, ?, ?)'''
+    cur = conn.cursor()
+    cur.execute(sql, transaction)
+    conn.commit()
+    return cur.lastrowid
